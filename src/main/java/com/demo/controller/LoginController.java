f75f7f1736ff6e0073995935c12c5de6880e94b0
tp6hannah <hannah51125@gmail.com>
2018-10-14 14:26:21 +0900

    Spring Security Dialect

--- src/main/java/com/demo/controller/LoginController.java
+++ src/main/java/com/demo/controller/LoginController.java
@@ -1,10 +1,11 @@
 package com.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.demo.service.UserService;
@@ -20,8 +21,15 @@ public class LoginController {
 	 * 
 	 * @return name of the html => "login.html"
 	 */
-	@GetMapping(value = { "/", "/login" })
+	@GetMapping("/login")
 	public String login() {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+		if (!(auth instanceof AnonymousAuthenticationToken)) {
+
+			/* The user is logged in :) */
+			return "redirect:/";
+		}
 		return "login2";
 	}
 }
