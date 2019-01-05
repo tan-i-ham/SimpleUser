5b7c43af2893456178227cf6f3c46bbf88967fd0
tp6hannah <hannah51125@gmail.com>
2019-01-05 22:21:13 +0900

    TodoList Repository and Service class

---
+++ src/main/java/com/demo/service/TodoListService.java
@@ -0,0 +1,38 @@
+package com.demo.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.demo.model.TodoList;
+import com.demo.repository.TodoListRepository;
+
+@Service
+@Transactional(readOnly = true)
+public class TodoListService {
+	private static final Logger LOGGER =  LoggerFactory.getLogger(TodoListService.class);
+
+	private final TodoListRepository todoListRepository;
+
+	public TodoListService(TodoListRepository todoListRepository) {
+		this.todoListRepository = todoListRepository;
+	}
+
+	@Transactional(rollbackFor = Exception.class)
+	public List<TodoList> saveAll(List<TodoList> todoLists) {
+		LOGGER.info("Saving {}", todoLists);
+		return todoListRepository.saveAll(todoLists);
+	}
+
+	public List<TodoList> getAllCommentsForToday() {
+		LocalDate localDate = LocalDate.now();
+		return todoListRepository.findByCreatedYearAndMonthAndDay(
+				localDate.getYear(), 
+				localDate.getMonth().getValue(),
+				localDate.getDayOfMonth());
+	}
+}
