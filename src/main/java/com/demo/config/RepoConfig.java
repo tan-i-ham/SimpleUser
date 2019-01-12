5bfbe25f87a30968e77473121412cd0f37f67f4b
tp6hannah <hannah51125@gmail.com>
2019-01-12 15:36:48 +0900

    package reconstruct

--- src/main/java/com/demo/config/RepoConfig.java
+++ src/main/java/com/demo/config/RepoConfig.java
@@ -1,4 +1,4 @@
-package com.demo.configuration;
+package com.demo.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -1,31 +1,88 @@
-package com.demo.service;
+package com.demo.service.Impl;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import com.demo.model.Role;
 import com.demo.model.User;
+import com.demo.repository.RoleRepository;
+import com.demo.repository.UserRepository;
+import com.demo.service.UserService;
 
-public interface UserService {
-	void save(User user);
+@Service
+public class UserServiceImpl implements UserService, UserDetailsService {
 
-	void edit(User user);
+	@Autowired
+	private UserRepository userRepository;
 
-	void delete(Long id);
+	@Autowired
+	private RoleRepository roleRepository;
 
-	List<User> getUserList();
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	Optional<User> findUserById(Long id);
+	@Override
+	public void save(User user) {
+		userRepository.save(user);
+	}
+
+	@Override
+	public void edit(User user) {
+		userRepository.save(user);
+	}
+
 	
-	User findUserByUsername(String name);
 	
-	User findUserByEmail(String email);
+	@Override
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+
+	@Override
+	public List<User> getUserList() {
+		return userRepository.findAll();
+	}
+
+	public Optional<User> findUserById(Long id) {
+		return userRepository.findById(id);
+	}
+
+	@Override
+	public User findUserByUsername(String name) {
+		return userRepository.findByUsername(name);
+	}
+
+	@Override
+	public User findUserByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+
+	public void saveUser(User user) {
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		user.setActive(1);
+		Role userRole = roleRepository.findByRole("ROLE_USER");
+		user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+		userRepository.save(user);
+	}
 	
-	void saveUser(User user);
-
-	List<User> findAll();
 
 
+	@Override
+	public UserDetails loadUserByUsername(String username) {
+		return (UserDetails) userRepository.findByUsername(username);
+	}
 
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		return userRepository.findAll();
+	}
 }
