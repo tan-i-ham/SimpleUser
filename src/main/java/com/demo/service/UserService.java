0754363ca117f9fb28427ba0078902fedfd2f80e
tp6hannah <hannah51125@gmail.com>
2019-01-12 15:38:26 +0900

    delete UserServiceImpl, left only UserService

--- src/main/java/com/demo/service/UserService.java
+++ src/main/java/com/demo/service/UserService.java
@@ -1,4 +1,4 @@
-package com.demo.service.Impl;
+package com.demo.service;
 
 import java.util.Arrays;
 import java.util.HashSet;
@@ -6,19 +6,23 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.demo.model.Role;
 import com.demo.model.User;
 import com.demo.repository.RoleRepository;
 import com.demo.repository.UserRepository;
-import com.demo.service.UserService;
 
 @Service
-public class UserServiceImpl implements UserService, UserDetailsService {
+@Transactional(readOnly = true)
+public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserRepository userRepository;
@@ -29,24 +33,19 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 	@Autowired
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void save(User user) {
 		userRepository.save(user);
 	}
 
-	@Override
 	public void edit(User user) {
 		userRepository.save(user);
 	}
 
-	
-	
-	@Override
 	public void delete(Long id) {
 		userRepository.deleteById(id);
 	}
 
-	@Override
 	public List<User> getUserList() {
 		return userRepository.findAll();
 	}
@@ -55,32 +54,36 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 		return userRepository.findById(id);
 	}
 
-	@Override
 	public User findUserByUsername(String name) {
 		return userRepository.findByUsername(name);
 	}
 
-	@Override
 	public User findUserByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
 
+	@Transactional(rollbackFor = Exception.class)
 	public void saveUser(User user) {
+//		user.setPassword(user.getPassword());
 		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 		user.setActive(1);
 		Role userRole = roleRepository.findByRole("ROLE_USER");
 		user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
 		userRepository.save(user);
 	}
-	
-
 
 	@Override
-	public UserDetails loadUserByUsername(String username) {
-		return (UserDetails) userRepository.findByUsername(username);
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepository.findByUsername(username);
+		if (user == null) {
+			throw new UsernameNotFoundException(username);
+		}
+		Role[] convertRole = user.getRoles().stream().toArray(Role[]::new);
+
+		return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+				Arrays.asList(new SimpleGrantedAuthority(convertRole[0].getRole())));
 	}
 
-	@Override
 	public List<User> findAll() {
 		// TODO Auto-generated method stub
 		return userRepository.findAll();
